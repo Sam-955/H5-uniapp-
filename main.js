eacf03d4c4eca61500abf007fe84b803627bf164
CBY <1091304279@qq.com>
2022-10-19 13:44:48 +0800

    首页开发完成

---
+++ main.js
@@ -0,0 +1,31 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+import {$http} from "@escook/request-miniprogram"
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+uni.$http=$http
+uni.$showMsg=function(title="数据加载失败",duration=1000){
+	uni.showToast({
+		duration,
+		title,
+		icon:'none'
+	})
+}
+$http.baseUrl='https://www.uinav.com'
+
+$http.beforeRequest=function(option){
+	uni.showLoading({
+		"title":"数据加载中....."
+	})
+}
+$http.afterRequest=function(){
+	uni.hideLoading()
+}
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
